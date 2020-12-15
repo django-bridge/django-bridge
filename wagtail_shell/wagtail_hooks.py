25ab7173d59f63b72757b3730bf196507d45fb22
Karl Hobley <karl@kaed.uk>
2020-12-15 20:11:33 +0000

    Pull in Wagtail Shell only on bootstrap template

--- wagtail_shell/wagtail_hooks.py
+++ wagtail_shell/wagtail_hooks.py
@@ -1,8 +1,6 @@
-from django.urls import path, include, reverse
-from django.utils.html import format_html
+from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
-from wagtail.admin.staticfiles import versioned_static
 from wagtail.core import hooks
 
 
@@ -10,9 +8,6 @@ from wagtail.core import hooks
 def register_admin_urls():
     urls = [
         path('jsi18n/', JavaScriptCatalog.as_view(packages=['wagtail_shell']), name='javascript_catalog'),
-
-        # Add your other URLs here, and they will appear under `/admin/shell/`
-        # Note: you do not need to check for authentication in views added here, Wagtail does this for you!
     ]
 
     return [
@@ -24,12 +19,3 @@ def register_admin_urls():
             ),
         )
     ]
-
-
-@hooks.register("insert_global_admin_js")
-def insert_global_admin_js():
-    return format_html(
-        '<script src="{}"></script><script src="{}"></script>',
-        versioned_static(reverse('wagtail_shell:javascript_catalog')),
-        versioned_static('wagtail_shell/js/wagtail-shell.js'),
-    )
