3d8e33d7647b089b4531fa5d58127e26b3c3a847
Karl Hobley <karl@kaed.uk>
2020-12-18 09:30:54 +0000

    Got logo animations working again

--- wagtail_shell/static_src/components/Logo.tsx
+++ wagtail_shell/static_src/components/Logo.tsx
@@ -15,10 +15,46 @@ interface LogoProps {
 }
 
 export const Logo: React.FunctionComponent<LogoProps> = ({images, homeUrl, navigate}) => {
+    // Tail wagging
+    // If the pointer changes direction 8 or more times without leaving, wag the tail!
+    const lastMouseX = React.useRef(0);
+    const lastDir = React.useRef<'r' | 'l'>('r');
+    const dirChangeCount = React.useRef(0);
+    const [isWagging, setIsWagging] = React.useState(false);
+
+
     const onClick = (e: React.MouseEvent) => {
         e.preventDefault();
         navigate(homeUrl);
     };
+
+    const onMouseMove = (e: React.MouseEvent) => {
+        const mouseX = e.pageX;
+        const dir: 'r' | 'l' = (mouseX > lastMouseX.current) ? 'r' : 'l';
+
+        if (mouseX != lastMouseX.current && dir != lastDir.current) {
+            dirChangeCount.current += 1;
+        }
+
+        if (dirChangeCount.current > 8) {
+            setIsWagging(true);
+        }
+
+        lastMouseX.current = mouseX;
+        lastDir.current = dir;
+    };
+
+    const onMouseLeave = () => {
+        setIsWagging(false);
+        dirChangeCount.current = 0;
+    };
+
+    const desktopClassNames = ["wagtail-logo-container__desktop", "u-hidden@xs"];
+    if (isWagging) {
+        desktopClassNames.push('logo-playful');
+    } else {
+        desktopClassNames.push('logo-serious');
+    }
 
     return (
         <a href="#" onClick={onClick} className="logo" aria-label={gettext('Dashboard')}>
@@ -28,7 +64,7 @@ export const Logo: React.FunctionComponent<LogoProps> = ({images, homeUrl, navig
             </div>
 
             {/* Desktop (animated) */}
-            <div className="wagtail-logo-container__desktop u-hidden@xs" data-animated-logo-container>
+            <div className={desktopClassNames.join(' ')} onMouseMove={onMouseMove} onMouseLeave={onMouseLeave}>
                 <div className="wagtail-logo-container-inner">
                     <img className="wagtail-logo wagtail-logo__body" src={images.desktopLogoBody} alt=""/>
                     <img className="wagtail-logo wagtail-logo__tail" src={images.desktopLogoTail} alt="" />
