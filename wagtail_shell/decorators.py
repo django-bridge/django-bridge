7ed5da1e61523a0ba5f23f9d075826901a4220ef
Karl Hobley <karl@kaed.uk>
2020-12-19 18:48:52 +0000

    Switch middleware for decorator

--- wagtail_shell/decorators.py
+++ wagtail_shell/decorators.py
@@ -1,14 +1,38 @@
 import functools
 
+from django.shortcuts import render
+
+from .response import ShellResponse, ShellResponseLoadIt, convert_to_shell_response
+
 
 def shell_enable(fn):
     """
-    Enables Wagtail shell on the given view
+    Enables Wagtail shell on the given view.
     """
     @functools.wraps(fn)
     def wrapper(request, *args, **kwargs):
-        request.wagtail_shell_enable = True
-        return fn(request, *args, **kwargs)
+        response = fn(request, *args, **kwargs)
+
+        # Attempt to convert non-shell response into a shell response
+        if response.status_code == 200 and not isinstance(response, ShellResponse) and not getattr(request, 'wagtail_shell_disable', False) and not request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+            response = convert_to_shell_response(request, response)
+
+        # If the request was made by the shell (using `fetch()`, rather than a regular browser request)
+        if request.META.get('HTTP_X_REQUESTED_WITH') == 'WagtailShell':
+            if isinstance(response, ShellResponse):
+                return response
+            else:
+                # Response couldn't be converted into a shell response. Reload the page
+                return ShellResponseLoadIt()
+
+        # Regular browser request
+        if isinstance(response, ShellResponse):
+            # Wrap the response with our shell bootstrap template
+            return render(request, 'wagtailshell/bootstrap.html', {
+                'data': response.content.decode('utf-8'),
+            })
+        else:
+            return response
 
     return wrapper
 
@@ -19,7 +43,7 @@ def shell_disable(fn):
     """
     @functools.wraps(fn)
     def wrapper(request, *args, **kwargs):
-        request.wagtail_shell_enable = False
+        request.wagtail_shell_disable = True
         return fn(request, *args, **kwargs)
 
     return wrapper
---
+++ wagtail_shell/response.py
@@ -0,0 +1,63 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+
+class ShellResponse(JsonResponse):
+    status = None
+
+    def __init__(self, *args, **kwargs):
+        data = {
+            'status': self.status,
+        }
+        data.update(self.get_data(*args, **kwargs))
+        super().__init__(data)
+        self['X-WagtailShellStatus'] = self.status
+
+    def get_data(self):
+        return {}
+
+
+class ShellResponseLoadIt(ShellResponse):
+    status = 'load-it'
+
+
+class ShellResponseRender(ShellResponse):
+    status = 'render'
+
+    def get_data(self, view, context):
+        return {
+            'view': view,
+            'context': context,
+        }
+
+
+class ShellResponseRenderHtml(ShellResponseRender):
+    def get_data(self, html):
+        return super().get_data('iframe', {'html': html})
+
+
+class ShellResponseNotFound(ShellResponse):
+    status = 'not-found'
+
+
+class ShellResponsePermissionDenied(ShellResponse):
+    status = 'permission-denied'
+
+
+def convert_to_shell_response(request, response):
+    """
+    Converts a non-shell response into a shell one.
+    """
+    # If the response is HTML, and our replacement admin_base.html was used,
+    # return a "render HTML" response that wraps the response in an iframe on the frontend
+
+    # FIXME: Find a proper mime type parser
+    is_html = response.get('Content-Type').startswith('text/html')
+    if is_html:
+        if hasattr(response, 'render'):
+            response.render()
+
+        return ShellResponseRenderHtml(response.content.decode('utf-8'))
+
+    # Can't convert the response
+    return response
