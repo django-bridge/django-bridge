4432385710e50763d1e44e1368cc8559fc525566
Karl Hobley <karl@kaed.uk>
2023-12-28 21:11:30 +0000

    Remove loading view

--- client/packages/core/src/views/Loading.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/require-default-props */
-
-import React, { ReactElement } from "react";
-
-function LoadingView(): ReactElement {
-    return <div />;
-}
-
-export default LoadingView;
