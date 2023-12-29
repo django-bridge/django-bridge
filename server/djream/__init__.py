c9b0cc1546cdb0f252c375796a9d5f086814c504
Karl Hobley <karl@kaed.uk>
2023-12-29 12:27:45 +0000

    Set license to BSD-3-Clause and change versions to 0.1a0

--- server/djream/__init__.py
+++ server/djream/__init__.py
@@ -1,2 +1,18 @@
-VERSION = (0, 1, 0)
-__version__ = ".".join(map(str, VERSION))
+def format_version(version):
+    if version[2] == 0:
+        # Ignore patch if it's 0
+        formatted = ".".join(str(x) for x in VERSION[:2])
+    else:
+        formatted = ".".join(str(x) for x in VERSION[:3])
+
+    if version[3] != "final":
+        mapping = {"alpha": "a", "beta": "b", "rc": "rc", "dev": ".dev"}
+        formatted += mapping[version[3]] + str(version[4])
+
+    return formatted
+
+
+VERSION = (0, 1, 0, "alpha", 0)
+__version__ = format_version(VERSION)
+
+print(__version__)
--- server/setup.py
+++ server/setup.py
@@ -2,9 +2,8 @@
 
 from os import path
 
-from setuptools import find_packages, setup
-
 from djream import __version__
+from setuptools import find_packages, setup
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "../README.md"), encoding="utf-8") as f:
@@ -21,7 +20,7 @@ setup(
     url="",
     packages=find_packages(),
     include_package_data=True,
-    license="BSD",
+    license="BSD-3-Clause",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
