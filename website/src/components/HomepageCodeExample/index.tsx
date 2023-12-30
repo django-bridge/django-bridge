fc6ee07f4c761d13944f63fb756e475307071a3d
Karl Hobley <karl@kaed.uk>
2023-12-30 13:43:54 +0000

    Improve code examples

--- website/src/components/HomepageCodeExample/index.tsx
+++ website/src/components/HomepageCodeExample/index.tsx
@@ -1,36 +1,102 @@
 import styles from './styles.module.css';
-import Step1 from '../../../docs/landing-intro/Step1.md'
-import Step2 from '../../../docs/landing-intro/Step2.md';
-import Step3 from '../../../docs/landing-intro/Step3.md';
+import CodeBlock from '@theme/CodeBlock';
 
 const steps = [
   {
     title: "Create a Djream-enabled view",
-    description: "Djream-enabled views return a DjreamResponse, that takes a component name and dictionary of props. Props can contain any JSON serializable value or object that has a JavaScript equivalent class.",
-    code: <Step1 />
+    description: "Djream-enabled views return a DjreamResponse containing the component name and dictionary of props. Props can contain any JSON serializable value or object that has a JavaScript equivalent class.",
+    code: <><CodeBlock language="python" title="views.py">
+      {`@djream_view
+def post_edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+
+    if form.is_valid():
+        form.save()
+        return redirect("post_detail", post_id=post.id)
+
+    return DjreamResponse(request, "PostForm", {
+        "title": post.title,
+        "form": form,
+        "form_action": reverse("post_edit", args=[post.id]),
+    })`}
+      </CodeBlock>
+      <CodeBlock language="python" title="urls.py">
+      {`urlpatterns = [
+    ...
+    path("posts/<int:post_id>/edit/", views.post_edit, name="post_edit"),
+]`}
+      </CodeBlock>
+      </>
   },
   {
     title: "Create a React component to render the view",
-    description: "The component will receive the props from the DjreamResponse.",
-    code: <Step2 />
+    description: "The component will receive the props from the DjreamResponse. Global props can defined which are passed to all components. For example the csrf_token is passed to all components by default.",
+    code: <CodeBlock language="tsx" title="PostForm.tsx">{`import React from 'react';
+import { Form, FormDef } from "@djream/forms";
+import { Button } from "@djream/ui";
+
+interface PostFormViewProps {
+  title: string;
+  form: FormDef;
+  form_action: string;
+  csrf_token: string;
+}
+
+export default function PostFormView({ title, form, form_action, csrf_token }: PostFormViewProps) {
+  return (
+    <>
+      <h1>{title}</h1>
+
+      <Form action={form_action} method="post">
+        <input type="hidden" name="csrfmiddlewaretoken" value={csrf_token} />
+        {form.render()}
+        <Button type="submit">Save</Button>
+      </Form>
+    </>
+  );
+}`}</CodeBlock>
   },
   {
-    title: "Add the component to App.jsx",
-    description: "Add the component to the views map in App.jsx.",
-    code: <Step3 />
+    title: "Add the component to the views map in App.tsx",
+    description: "Pass a map of component names to components to the Shell component. The Shell component will render the component that matches the name in the DjreamResponse.",
+    code: <CodeBlock language="tsx" title="App.tsx">{`import Shell, { ShellResponse } from "@djream/core";
+import PostFormView from "./PostForm";
+
+// List of views that can be rendered by Djream
+const views = new Map();
+views.set("PostForm", PostFormView);
+
+function App(): ReactElement {
+  // Get the initial response from the server
+  // This is used to render the first view, subsequent views are loaded via AJAX
+  const rootElement = document.getElementById("root");
+  const initialResponse = rootElement.dataset.initialResponse;
+
+  return (
+    <Shell
+      views={views}
+      initialResponse={JSON.parse(initialResponse)}
+    />
+  );
+}
+
+export default App;`}</CodeBlock>
   }
 ];
 
 export default function HomepageCodeExample(): JSX.Element {
   return (
     <section className={styles.codeexample}>
-      {steps.map((step, idx) => (
-        <div className={styles.step} key={idx}>
-          <h3>{step.title}</h3>
-          <p>{step.description}</p>
-          <pre>{step.code}</pre>
-        </div>
-      ))}
+      <ol className={styles.steps}>
+        {steps.map((step, idx) => (
+          <li key={idx}>
+            <h3>{step.title}</h3>
+            <p>{step.description}</p>
+            {step.code}
+          </li>
+        ))}
+      </ol>
     </section>
   );
 }
