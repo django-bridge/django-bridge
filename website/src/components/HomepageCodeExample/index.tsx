472b1170cefcf7f63fccb725d9f8e46d6e13ab24
Karl Hobley <karl@kaed.uk>
2023-12-30 17:41:48 +0000

    Make code examples look better on mobile

--- website/src/components/HomepageCodeExample/index.tsx
+++ website/src/components/HomepageCodeExample/index.tsx
@@ -24,7 +24,11 @@ def post_edit(request, post_id):
       <CodeBlock language="python" title="urls.py">
       {`urlpatterns = [
     ...
-    path("posts/<int:post_id>/edit/", views.post_edit, name="post_edit"),
+
+    path(
+        "posts/<int:post_id>/edit/",
+        views.post_edit, name="post_edit"
+    ),
 ]`}
       </CodeBlock>
       </>
@@ -32,24 +36,22 @@ def post_edit(request, post_id):
   {
     title: "Create a React component to render the view",
     description: "The component will receive the props from the DjreamResponse. Global props can defined which are passed to all components. For example the csrf_token is passed to all components by default.",
-    code: <CodeBlock language="tsx" title="PostForm.tsx">{`import React from 'react';
-import { Form, FormDef } from "@djream/forms";
-import { Button } from "@djream/ui";
-
-interface PostFormViewProps {
-  title: string;
-  form: FormDef;
-  form_action: string;
-  csrf_token: string;
-}
-
-export default function PostFormView({ title, form, form_action, csrf_token }: PostFormViewProps) {
+    code: <CodeBlock language="tsx" title="PostForm.tsx">{`export default function PostFormView({
+  title,
+  form,
+  form_action,
+  csrf_token
+}: PostFormViewProps) {
   return (
     <>
       <h1>{title}</h1>
 
       <Form action={form_action} method="post">
-        <input type="hidden" name="csrfmiddlewaretoken" value={csrf_token} />
+        <input
+          type="hidden"
+          name="csrfmiddlewaretoken"
+          value={csrf_token}
+        />
         {form.render()}
         <Button type="submit">Save</Button>
       </Form>
@@ -59,22 +61,20 @@ export default function PostFormView({ title, form, form_action, csrf_token }: P
   },
   {
     title: "Add the component to the views map in App.tsx",
-    description: "Pass a map of component names to components to the Shell component. The Shell component will render the component that matches the name in the DjreamResponse.",
-    code: <CodeBlock language="tsx" title="App.tsx">{`import Shell, { ShellResponse } from "@djream/core";
-import PostFormView from "./PostForm";
-
-// List of views that can be rendered by Djream
+    description: "Pass a map of view components to the DjreamShell, this will render the component that matches the name in the DjreamResponse.",
+    code: <CodeBlock language="tsx" title="App.tsx">{`// List of views that can be rendered by Djream
 const views = new Map();
 views.set("PostForm", PostFormView);
 
 function App(): ReactElement {
   // Get the initial response from the server
-  // This is used to render the first view, subsequent views are loaded via AJAX
+  // This is used to render the first view,
+  // subsequent views are loaded via AJAX
   const rootElement = document.getElementById("root");
   const initialResponse = rootElement.dataset.initialResponse;
 
   return (
-    <Shell
+    <DjreamShell
       views={views}
       initialResponse={JSON.parse(initialResponse)}
     />
