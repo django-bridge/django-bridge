c8eb864c9d88503d67a01418418daad468acb04b
Karl Hobley <karl@kaed.uk>
2024-01-04 22:33:01 +0000

    Change the title again

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "Djream",
+  title: "Djream: a Django/React SPA Framework",
   tagline:
     "Rapidly build lightweight React SPAs with Django views and forms.",
   favicon: "img/favicon.ico",
