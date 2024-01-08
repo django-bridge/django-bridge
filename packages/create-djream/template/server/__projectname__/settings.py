b4af3e96362cec989e71f933a004aae97dbfcb78
Karl Hobley <karl@kaed.uk>
2024-01-08 20:55:03 +0000

    Add PostgreSQL to project template

--- packages/create-djream/template/server/__projectname__/settings.py
+++ packages/create-djream/template/server/__projectname__/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +78,9 @@ WSGI_APPLICATION = "__projectname__.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3"
+    )
 }
 
 
