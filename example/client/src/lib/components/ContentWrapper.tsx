0d187e52694ba3eae0b715413dd7b55f54a20c14
Karl Hobley <karl@kaed.uk>
2024-01-08 21:00:03 +0000

    Delete old example project

--- example/client/src/lib/components/ContentWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-`;
-
-export default ContentWrapper;
--- example/client/src/utils/url.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export function appendNextParam(url: string): string {
-  const params = new URLSearchParams({
-    next: window.location.pathname,
-  });
-
-  return `${url}?${params.toString()}`;
-}
