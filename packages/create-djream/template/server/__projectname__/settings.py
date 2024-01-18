6b69d3da3c1131afab1fe86ce5c3db485dd61a2a
Karl Hobley <karl@kaed.uk>
2024-01-18 21:54:00 +0000

    Add a customised bootstrap.json

--- packages/create-djream/template/server/__projectname__/settings.py
+++ packages/create-djream/template/server/__projectname__/settings.py
@@ -53,7 +53,9 @@ ROOT_URLCONF = "__projectname__.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "__projectname__" / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
