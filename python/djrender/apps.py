5368c9a4397eadfe66c614190fbb8349ad60ff5f
Karl Hobley <karl@kaed.uk>
2024-03-20 12:36:52 +0000

    Remove 'render' sub-app

--- python/djrender/apps.py
+++ python/djrender/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class DjangoRenderAppConfig(AppConfig):
     label = "djrenderrender"
-    name = "djrender.render"
+    name = "djrender"
     verbose_name = "Django Render"
     default_auto_field = "django.db.models.BigAutoField"
 
