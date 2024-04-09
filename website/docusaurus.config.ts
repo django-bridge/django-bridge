067915c27bf94e980670be1538d355b5ecb74db4
Karl Hobley <karl@kaed.uk>
2024-04-09 17:33:58 +0000

    Tweak wording for better readability

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 const config: Config = {
   title: "Django Render",
   tagline:
-    "Django Render provides a streamlined approach for building React* Frontends for Django applications",
+    "Django Render provides a streamlined approach for building Django applications with React* Frontends",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
