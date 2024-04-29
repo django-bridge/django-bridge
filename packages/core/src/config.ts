e9729f873bca04cbdf58187ac545319d6a16f335
Karl Hobley <karl@kaed.uk>
2024-04-29 10:11:33 +0000

    Context providers

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -1,14 +1,17 @@
-import { FunctionComponent } from "react";
+import { Context, FunctionComponent } from "react";
 import Telepath from "telepath-unpack";
 
 export default class Config {
   public views: Map<string, FunctionComponent>;
+
+  public contextProviders: Map<string, Context<unknown>>;
 
   // Telepath Doesn't support typescript yet
   public telepathRegistry: Telepath;
 
   constructor() {
     this.views = new Map();
+    this.contextProviders = new Map();
     this.telepathRegistry = new Telepath();
 
     // Add default deserializers
@@ -20,6 +23,14 @@ export default class Config {
     component: FunctionComponent<P>
   ): Config => {
     this.views.set(name, component as FunctionComponent<object>);
+    return this;
+  };
+
+  public addContextProvider = <C>(
+    name: string,
+    context: Context<C>
+  ): Config => {
+    this.contextProviders.set(name, context as Context<unknown>);
     return this;
   };
 
