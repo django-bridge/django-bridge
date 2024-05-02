e211b455735eee8e625ba08493d28f7764f4ef53
Karl Hobley <karl@kaed.uk>
2024-05-02 12:09:48 +0000

    Add trailing slashes
    To fix SEO issue as Cloudflare pages does this with redirects

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -14,6 +14,8 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
+  trailingSlash: true,
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "kaedroho", // Usually your GitHub org/user name.
