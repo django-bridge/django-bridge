e58103e3a21e001b80dce8a9aed7416b2527d6e8
Karl Hobley <karl@kaed.uk>
2024-05-25 15:10:16 +0000

    Delete default widget adapters

--- python/django_render/adapters/forms.py
+++ python/django_render/adapters/forms.py
@@ -1,98 +1,8 @@
-"""
-Register Telepath adapters for core Django form widgets, so that they can
-have corresponding JavaScript objects with the ability to render new instances
-and extract field values.
-"""
-
 from datetime import date, datetime, time
 
 from django import forms
-from django.conf import settings
-from django.forms.models import ModelChoiceIteratorValue
-from django.template.defaultfilters import filesizeformat
-from telepath import ValueNode
 
 from .registry import Adapter, register, registry
-
-
-class TextInputAdapter(Adapter):
-    js_constructor = "forms.TextInput"
-
-    def js_args(self, widget):
-        return [
-            "text",
-            widget.id_for_label("__ID__"),
-            widget.attrs.get("class", ""),
-        ]
-
-
-register(TextInputAdapter(), forms.TextInput)
-
-
-class PasswordInputAdapter(Adapter):
-    js_constructor = "forms.TextInput"
-
-    def js_args(self, widget):
-        return [
-            "password",
-            widget.id_for_label("__ID__"),
-            widget.attrs.get("class", ""),
-        ]
-
-
-register(PasswordInputAdapter(), forms.PasswordInput)
-
-
-class FileInputAdapter(Adapter):
-    js_constructor = "forms.FileInput"
-
-    def js_args(self, widget):
-        return [
-            widget.id_for_label("__ID__"),
-            widget.attrs.get("class", ""),
-            widget.attrs.get("accept", ""),
-            filesizeformat(settings.MAX_UPLOAD_SIZE),
-        ]
-
-
-register(FileInputAdapter(), forms.FileInput)
-
-
-class SelectAdapter(Adapter):
-    js_constructor = "forms.Select"
-
-    def js_args(self, widget):
-        return [
-            widget.id_for_label("__ID__"),
-            widget.options("__NAME__", ""),
-            widget.attrs.get("class", ""),
-        ]
-
-
-register(SelectAdapter(), forms.Select)
-
-
-class SelectMultipleAdapter(Adapter):
-    js_constructor = "forms.SelectMultiple"
-
-    def js_args(self, widget):
-        return [
-            widget.id_for_label("__ID__"),
-            widget.options("__NAME__", ""),
-            widget.attrs.get("class", ""),
-            widget.attrs.get("inline", False),
-        ]
-
-
-register(SelectMultipleAdapter(), forms.SelectMultiple)
-
-
-class ModelChoiceIteratorValueAdapter(Adapter):
-    def build_node(self, value, context):
-        return ValueNode(value.value)
-
-
-register(ModelChoiceIteratorValueAdapter(), ModelChoiceIteratorValue)
 
 
 class FieldWithName:
@@ -114,7 +24,7 @@ class FieldAdapter(Adapter):
                     field.field.field.disabled,
                     field.field.help_text,
                     field.field.field.widget.render(field.name, field.field.value()),
-                ]
+                ],
             )
 
         # Render widget on the client
@@ -133,7 +43,7 @@ class FieldAdapter(Adapter):
                 field.field.field.widget,
                 field.field.help_text,
                 value,
-            ]
+            ],
         )
 
 
