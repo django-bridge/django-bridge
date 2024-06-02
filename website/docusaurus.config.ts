7231f7f21ba2bda50d5528a3535cd36cd1c9ea6f
Karl Hobley <karl@kaed.uk>
2024-06-02 11:08:33 +0000

    Add a social card

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -50,8 +50,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/social-card.png",
     navbar: {
       logo: {
         alt: "Django Render",
